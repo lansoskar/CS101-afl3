fc7a24dfd858987ac2eaeeaee1153344848b5495
Oskar <lansoskar@gmail.com>
2023-02-14 14:12:22 +0100

    ini

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Article> articles = new ArrayList<>();
-/*        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             RandomNum.randomNum();
         }
-        CPRcheck.checkCPR();*/
+        CPRcheck.checkCPR();
         Article a = new Article("Rockerklub flyttede ind i nedlagt brugs: Nu er de sparket ud af byen ", "Anders Malling Beck");
         Article b = new Article("Efter massiv shitstorm: Unisport laver ny salgsrekord ", "Sebastian Frølich");
         Article c = new Article("Bilgigant varsler massefyringer ", "Anne Katrine Restrup");
