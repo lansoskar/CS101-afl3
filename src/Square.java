5064e63717cf50e704152a5936e84ba640fd59f2
Oskar <lansoskar@gmail.com>
2023-02-16 09:56:48 +0100

    note added

--- src/Square.java
+++ src/Square.java
@@ -17,5 +17,16 @@ public class Square implements Comparable<Square>{
     public int compareTo(Square o) {
         System.out.println(Integer.compare(this.getPerim(), o.getPerim()));
         return Integer.compare(this.getPerim(), o.getPerim());
+
+        /*
+        ^ this is the same as writing - downside er ingen customizations
+        if (this.getPerim() > o.getPerim()) {
+            return 1;
+        } else if (this.getPerim() == o.getPerim()) {
+            return 0;
+        } else if (this.getPerim() < o.getPerim()){
+            return -1;
+        }
+         */
     }
 }
